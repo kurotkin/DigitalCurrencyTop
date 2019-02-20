36058e3f728fff30433a0abfabbb9c82fe0bbe90
kurotkin <kurotkin@users.noreply.github.com>
2019-02-20 21:31:22 +0300

    Added about activity

---
+++ app/src/main/java/com/kurotkin/digitalcurrencytop/activities/AboutActivity.kt
@@ -0,0 +1,13 @@
+package com.kurotkin.digitalcurrencytop.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kurotkin.digitalcurrencytop.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
