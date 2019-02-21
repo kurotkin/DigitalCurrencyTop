1862b94e2ffbc02328c65a6cd939ee6ddb66e680
Vitaly Kurotkin <example@example.com>
2019-02-21 16:19:20 +0300

    Added ad block

--- app/src/main/java/com/kurotkin/digitalcurrencytop/MainActivity.kt
+++ app/src/main/java/com/kurotkin/digitalcurrencytop/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.kurotkin.digitalcurrencytop
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.kurotkin.digitalcurrencytop.activities.AboutActivity
+
+private lateinit var mInterstitialAd: InterstitialAd
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-1279324864785766/1372208456"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId) {
+            R.id.action_about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAd() {
+        if (mInterstitialAd.isLoaded) {
+            mInterstitialAd.show()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        showAd()
     }
 }
--- app/src/main/java/com/kurotkin/digitalcurrencytop/activities/AboutActivity.kt
+++ app/src/main/java/com/kurotkin/digitalcurrencytop/activities/AboutActivity.kt
@@ -1,8 +1,12 @@
 package com.kurotkin.digitalcurrencytop.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
 import com.kurotkin.digitalcurrencytop.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
@@ -10,6 +14,18 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        // Стрелка возврата в тулбаре
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Открывать страницу приложения в маркете GooglePlay
+        buttonRateApp.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse("market://details?id=${applicationContext.packageName}")))
+        }
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
+
+
 }
