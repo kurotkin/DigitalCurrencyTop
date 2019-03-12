9eb4791cb4e2608b5fab5568ad9eefe26cb10adc
Vitaly Kurotkin <example@example.com>
2019-03-12 15:41:55 +0300

    Added api and layout

---
+++ app/src/main/java/com/kurotkin/digitalcurrencytop/CoinGeckoApi.kt
@@ -0,0 +1,27 @@
+package com.kurotkin.digitalcurrencytop
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import io.reactivex.Observable
+
+interface CoinGeckoApi {
+
+    //запрос списка криптовалют
+    @GET("coins/markets")
+    fun getCoinMarket(
+        @Query("vs_currency") vs: String = "usd",
+        @Query("per_page") perPage: Int = 100,
+        @Query("sparkline") sparkline: Boolean = false,
+        @Query("order") order: String = "market_cap_desc"
+    ): Observable<List<GeckoCoin>>
+
+
+    //запрос данных для графика
+    @GET("coins/{id}/market_chart")
+    fun getCoinMarketChart(
+        @Path("id") id: String,
+        @Query("vs_currency") vsCurrency: String = "usd",
+        @Query("days") days: String = "max"
+    ): Observable<GeckoCoinChart>
+}
